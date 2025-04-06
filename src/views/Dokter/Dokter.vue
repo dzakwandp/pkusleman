fa52da4e99d8e9bc20f8ec90e1a0a91e5595fd1b
Dzakwan Diego <diegodzakwan@gmail.com>
2025-04-07 06:39:09 +0700

    change

--- src/views/Dokter/Dokter.vue
+++ src/views/Dokter/Dokter.vue
@@ -1,29 +1,37 @@
 <template>
-  <div class="flex px-20 mt-10">
-    <FlipCard v-for="item in dokter" :key="item.index" class="mr-6">
-      <template #default>
-        <div
-          class="flex h-full items-end"
-          :style="{ backgroundImage: `url(${bgDokter})` }"
-          style="background-size: cover">
-          <img :src="'https://apiweb.pkusleman.com/' + item.gambar" />
-        </div>
-      </template>
-      <template #back>
-        <div>
-          <p>
-            {{
-              item.gelar_depan + " " + item.nama + ", " + item.gelar_belakang
-            }}
-          </p>
-          <div class="h-max w-max px-1 bg-white rounded-md">
-            <p class="text-sm font-semibold mt-2 text-primary-green">{{ item.spesialisasi }}</p>
+  <div class="flex flex-col px-20 mt-10">
+    <p class="text-4xl text-center text-gray-800">Para Dokter Kami.</p>
+    <p class="text-gray-800 text-center mb-10">
+      Para dokter kami yang selalu siaga dan berusaha memberikan pelayanan terbaik untuk membantu kesehatan Anda.
+    </p>
+    <div class="flex">
+      <FlipCard v-for="item in dokter" :key="item.index" class="mr-6">
+        <template #default>
+          <div
+            class="flex h-full items-end"
+            :style="{ backgroundImage: `url(${bgDokter})` }"
+            style="background-size: cover">
+            <img :src="'https://apiweb.pkusleman.com/' + item.gambar" />
           </div>
-          <div class="h-[2px] w-full bg-white my-2" />
-          <p class="text-xs">{{ item.tentang }}</p>
-        </div>
-      </template>
-    </FlipCard>
+        </template>
+        <template #back>
+          <div>
+            <p>
+              {{
+                item.gelar_depan + " " + item.nama + ", " + item.gelar_belakang
+              }}
+            </p>
+            <div class="h-max w-max px-1 bg-white rounded-md">
+              <p class="text-sm font-semibold mt-2 text-primary-green">
+                {{ item.spesialisasi }}
+              </p>
+            </div>
+            <div class="h-[2px] w-full bg-white my-2" />
+            <p class="text-xs">{{ item.tentang }}</p>
+          </div>
+        </template>
+      </FlipCard>
+    </div>
   </div>
 </template>
 
