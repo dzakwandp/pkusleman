49ebe5ce484000b3224fd6d8b88c1039c7cf618d
Dzakwan Diego <diegodzakwan@gmail.com>
2025-04-08 17:08:43 +0700

    [feat] dokter: add spelialisasi name

--- src/views/Dokter/Dokter.vue
+++ src/views/Dokter/Dokter.vue
@@ -1,36 +1,63 @@
 <template>
   <div class="flex flex-col px-20 mt-10">
     <p class="text-4xl text-center text-gray-800">Para Dokter Kami.</p>
-    <p class="text-gray-800 text-center mb-10">
-      Para dokter kami yang selalu siaga dan berusaha memberikan pelayanan terbaik untuk membantu kesehatan Anda.
+    <p class="text-gray-800 text-center">
+      Para dokter kami yang selalu siaga dan berusaha memberikan pelayanan
+      terbaik untuk membantu kesehatan Anda.
     </p>
-    <div class="flex">
-      <FlipCard v-for="item in dokter" :key="item.index" class="mr-6">
-        <template #default>
-          <div
-            class="flex h-full items-end"
-            :style="{ backgroundImage: `url(${bgDokter})` }"
-            style="background-size: cover">
-            <img :src="'https://apiweb.pkusleman.com/' + item.gambar" />
+    <p class="text-gray-800 italic mb-10 text-center text-sm">
+      *Untuk jadwal dokter secara lengkap dapat dilihat
+      <a
+        class="underline"
+        href="https://daftar.pkusleman.com/jadwal-dokter/"
+        target="_blank"
+        >disini</a
+      >
+    </p>
+    <div class="flex flex-col gap-6">
+      <div
+        v-for="spesial in spesialisasi"
+        :key="spesial.index"
+        class="flex gap-6">
+        <div class="flex flex-col w-1/4">
+          <p class="text-gray-800 text-3xl">{{ spesial.text }}</p>
+          <div class="w-full h-[2px] bg-primary-green"></div>
+        </div>
+        <div class="flex flex-wrap w-3/4">
+          <div v-for="item in dokter" :key="item.index">
+            <FlipCard v-if="spesial.text === item.spesialisasi" class="mr-6">
+              <template #default>
+                <div
+                  class="flex h-full items-end"
+                  :style="{ backgroundImage: `url(${bgDokter})` }"
+                  style="background-size: cover">
+                  <img :src="'https://apiweb.pkusleman.com/' + item.gambar" />
+                </div>
+              </template>
+              <template #back>
+                <div>
+                  <p>
+                    {{
+                      item.gelar_depan +
+                      " " +
+                      item.nama +
+                      ", " +
+                      item.gelar_belakang
+                    }}
+                  </p>
+                  <div class="h-max w-max px-1 bg-white rounded-md">
+                    <p class="text-sm font-semibold mt-2 text-primary-green">
+                      {{ item.spesialisasi }}
+                    </p>
+                  </div>
+                  <div class="h-[2px] w-full bg-white my-2" />
+                  <p class="text-xs">{{ item.tentang }}</p>
+                </div>
+              </template>
+            </FlipCard>
           </div>
-        </template>
-        <template #back>
-          <div>
-            <p>
-              {{
-                item.gelar_depan + " " + item.nama + ", " + item.gelar_belakang
-              }}
-            </p>
-            <div class="h-max w-max px-1 bg-white rounded-md">
-              <p class="text-sm font-semibold mt-2 text-primary-green">
-                {{ item.spesialisasi }}
-              </p>
-            </div>
-            <div class="h-[2px] w-full bg-white my-2" />
-            <p class="text-xs">{{ item.tentang }}</p>
-          </div>
-        </template>
-      </FlipCard>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -45,6 +72,7 @@ export default {
   },
   data() {
     return {
+      spesialisasi: [],
       dokter: [],
       bgDokter: bgDokter,
     };
@@ -56,7 +84,12 @@ export default {
           "https://apiweb.pkusleman.com/api/dokter"
         );
         this.dokter = dokter.data;
-        console.log(this.dokter);
+        this.spesialisasi = [
+          ...new Set(this.dokter.map((item) => item.spesialisasi)),
+        ]
+          .sort((a, b) => a.localeCompare(b))
+          .map((item) => ({ text: item }));
+        console.log(this.spesialisasi);
       } catch (err) {
         console.log(err);
       }
