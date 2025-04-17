aa58d801eadc5c90a1b59790aa54c06eacef4e55
Dzakwan Diego <diegodzakwan@gmail.com>
2025-04-17 14:17:58 +0700

    [feat] dokter: add filter for dokter

--- src/views/Dokter/Dokter.vue
+++ src/views/Dokter/Dokter.vue
@@ -15,49 +15,64 @@
       </a>
     </p>
     <div class="flex flex-col gap-6">
-      <div
-        v-for="spesial in spesialisasi"
-        :key="spesial.index"
-        class="flex gap-6">
-        <div class="flex flex-col w-1/4">
-          <p class="text-gray-800 text-3xl">{{ spesial.text }}</p>
-          <div class="w-full h-[2px] bg-primary-green"></div>
-        </div>
-        <div class="flex flex-wrap w-3/4">
-          <div v-for="item in dokter" :key="item.index">
-            <FlipCard v-if="spesial.text === item.group" class="mr-6 mt-6">
-              <template #default>
-                <div
-                  class="flex h-full items-end"
-                  :style="{ backgroundImage: `url(${bgDokter})` }"
-                  style="background-size: cover">
-                  <img :src="'https://apiweb.pkusleman.com/' + item.gambar" />
-                </div>
-              </template>
-              <template #back>
-                <div>
-                  <p>
-                    {{
-                      item.gelar_depan +
-                      " " +
-                      item.nama +
-                      ", " +
-                      item.gelar_belakang
-                    }}
-                  </p>
-                  <div class="h-max w-max px-1 bg-white rounded-md">
-                    <p class="text-sm font-semibold mt-2 text-primary-green">
-                      {{ item.spesialisasi }}
-                    </p>
-                  </div>
-                  <div class="h-[2px] w-full bg-white my-2" />
-                  <p class="text-xs">{{ item.tentang }}</p>
-                </div>
-              </template>
-            </FlipCard>
-          </div>
+      <div class="flex gap-4 justify-center sticky top-[6.5rem] z-10 bg-white py-2">
+        <div
+          v-for="item in spesialisasi"
+          :key="item.index"
+          @click="toggleFilter(item.text)"
+          class="p-2 cursor-pointer outline outline-1 outline-primary-green rounded-xl text-sm hover:bg-primary-green hover:text-white transition"
+          :class="{
+            'bg-primary-green text-white': selectedFilters.includes(item.text),
+            'bg-white text-gray-800': !selectedFilters.includes(item.text),
+          }">
+          {{ item.text }}
         </div>
       </div>
+      <TransitionGroup name="fade" tag="div">
+        <div
+          v-for="spesial in filteredSpesialisasi"
+          :key="spesial.index"
+          class="flex gap-6">
+          <div class="flex flex-col w-1/4">
+            <p class="text-gray-800 text-3xl">{{ spesial.text }}</p>
+            <div class="w-full h-[2px] bg-primary-green"></div>
+          </div>
+          <div class="flex flex-wrap w-3/4">
+            <div v-for="item in dokter" :key="item.index">
+              <FlipCard v-if="spesial.text === item.group" class="mr-6 mt-6">
+                <template #default>
+                  <div
+                    class="flex h-full items-end"
+                    :style="{ backgroundImage: `url(${bgDokter})` }"
+                    style="background-size: cover">
+                    <img :src="'https://apiweb.pkusleman.com/' + item.gambar" />
+                  </div>
+                </template>
+                <template #back>
+                  <div>
+                    <p>
+                      {{
+                        item.gelar_depan +
+                        " " +
+                        item.nama +
+                        ", " +
+                        item.gelar_belakang
+                      }}
+                    </p>
+                    <div class="h-max w-max px-1 bg-white rounded-md">
+                      <p class="text-sm font-semibold mt-2 text-primary-green">
+                        {{ item.spesialisasi }}
+                      </p>
+                    </div>
+                    <div class="h-[2px] w-full bg-white my-2" />
+                    <p class="text-xs">{{ item.tentang }}</p>
+                  </div>
+                </template>
+              </FlipCard>
+            </div>
+          </div>
+        </div>
+      </TransitionGroup>
     </div>
   </div>
 </template>
@@ -75,6 +90,8 @@ export default {
       spesialisasi: [],
       dokter: [],
       bgDokter: bgDokter,
+      selectedFilters: [],
+      filteredSpesialisasi: [],
     };
   },
   methods: {
@@ -84,15 +101,35 @@ export default {
           "https://apiweb.pkusleman.com/api/dokter"
         );
         this.dokter = dokter.data;
-        this.spesialisasi = [
-          ...new Set(this.dokter.map((item) => item.group)),
-        ]
+        this.spesialisasi = [...new Set(this.dokter.map((item) => item.group))]
           .sort((a, b) => a.localeCompare(b))
           .map((item) => ({ text: item }));
-        console.log(this.spesialisasi);
+        this.filteredSpesialisasi = this.spesialisasi;
       } catch (err) {
         console.log(err);
       }
+    },
+    toggleFilter(spesialis) {
+      const index = this.selectedFilters.indexOf(spesialis);
+      if (index === -1) {
+        this.selectedFilters.push(spesialis);
+      } else {
+        this.selectedFilters.splice(index, 1);
+      }
+    },
+  },
+  watch: {
+    selectedFilters: {
+      handler() {
+        if (this.selectedFilters.length === 0) {
+          this.filteredSpesialisasi = this.spesialisasi;
+        } else {
+          this.filteredSpesialisasi = this.spesialisasi.filter((doc) =>
+            this.selectedFilters.includes(doc.text)
+          );
+        }
+      },
+      deep: true,
     },
   },
   mounted() {
@@ -101,4 +138,13 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  @apply transition-all duration-300;
+}
+.fade-enter-from,
+.fade-leave-to {
+  @apply opacity-0 translate-y-4;
+}
+</style>
