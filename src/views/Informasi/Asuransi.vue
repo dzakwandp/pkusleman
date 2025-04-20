9787ad9310d6e5249a3804b47e17bb86c666a556
Dzakwan Diego <diegodzakwan@gmail.com>
2025-04-21 06:39:54 +0700

    [feat] asuransi: add asuransi page

--- src/views/Informasi/Asuransi.vue
+++ src/views/Informasi/Asuransi.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    Asuransi
+  <div class="flex flex-col px-20 mt-10 min-h-screen">
+    <p class="text-4xl text-center text-gray-800">Asuransi dan Rekanan.</p>
+    <p class="text-gray-800 text-center">
+      Para dokter kami yang selalu siaga dan berusaha memberikan pelayanan
+      terbaik untuk membantu kesehatan Anda.
+    </p>
   </div>
 </template>
 
