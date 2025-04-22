ec04e2e235925675983c7727269704b8139fe951
Dzakwan Diego <diegodzakwan@gmail.com>
2025-04-22 14:04:10 +0700

    [feat] 404: add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import Promo from "@/views/Informasi/Promo.vue";
 import ArtikelBerita from "@/views/Informasi/ArtikelBerita.vue";
 
 import Redir from "@/views/Redir.vue";
+import NotFound from "@/views/404.vue";
 // import igd from "@/views/layanan/igd.vue";
 // import rawatjalan from "@/views/layanan/rawatjalan.vue";
 // import rawatinap from "@/views/layanan/rawatinap.vue";
@@ -105,8 +106,13 @@ const router = createRouter({
     {
       path: "/artikel-berita",
       name: "artikel-berita",
-      component: ArtikelBerita
-    }
+      component: ArtikelBerita,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "404",
+      component: NotFound,
+    },
     // {
     //   path: "/layanan/igd",
     //   name: "igd",
---
+++ src/views/404.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col items-center min-h-screen">
+    <img :src="image" class="h-[75vh] mx-auto" />
+    <HoverButton
+      text="Back to Home"
+      class="text-sm w-44 text-gray-800 rounded-lg"
+      @click="this.$router.push('/')" />
+  </div>
+</template>
+
+<script>
+import image from "@/assets/images/404.png";
+import HoverButton from "@/components/inspira-ui/InteractiveHoverButton.vue";
+export default {
+  components: {
+    HoverButton,
+  },
+  data() {
+    return {
+      image: image,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
