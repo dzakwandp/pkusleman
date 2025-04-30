514a9eff631baea9bf8c8d2dfbb916443d2fe4cc
Dzakwan Diego <diegodzakwan@gmail.com>
2025-04-30 22:59:49 +0700

    [feat] contact: add responsive

--- src/views/Tentang/Contact.vue
+++ src/views/Tentang/Contact.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-col px-20">
-    <div class="flex flex-col mt-10 w-max mx-auto gap-2">
+  <div class="flex flex-col px-4 md:px-20">
+    <div class="flex flex-col mt-10 mb-10 md:mb-0 w-max mx-auto gap-2">
       <p class="text-4xl text-gray-800">Hubungi Kami</p>
       <div class="h-[3px] bg-primary-green"></div>
     </div>
@@ -16,42 +16,44 @@
           class="text-primary-green text-2xl" />
       </a>
     </div>
-    <table class="w-full mt-6">
-      <thead class="bg-primary-green text-white h-10">
-        <tr>
-          <th>No</th>
-          <th>Bagian</th>
-          <th>Nomor Telepon</th>
-          <th>Hari Kerja</th>
-          <th>Jam Kerja</th>
-          <th>Nomor Whatsapp (Hanya Chat)</th>
-          <th>Hari Kerja</th>
-          <th>Jam Kerja</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="item in contact"
-          :key="item.index"
-          class="text-center h-10 hover:bg-black/10"
-          :class="trClass(item.id)">
-          <td>{{ item.id }}</td>
-          <td>{{ item.bagian }}</td>
-          <td>{{ item.telp }}</td>
-          <td>{{ item.hari_telp }}</td>
-          <td>{{ item.jam_telp }}</td>
-          <td>{{ item.whatsapp }}</td>
-          <td>{{ item.hari_whatsapp }}</td>
-          <td>{{ item.jam_whatsapp }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="overflow-auto">
+      <table class="w-full mt-6">
+        <thead class="bg-primary-green text-white h-10 text-nowrap">
+          <tr>
+            <th class="px-2">No</th>
+            <th class="px-2">Bagian</th>
+            <th class="px-2">Nomor Telepon</th>
+            <th class="px-2">Hari Kerja</th>
+            <th class="px-2">Jam Kerja</th>
+            <th class="px-2">Nomor Whatsapp (Hanya Chat)</th>
+            <th class="px-2">Hari Kerja</th>
+            <th class="px-2">Jam Kerja</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="item in contact"
+            :key="item.index"
+            class="text-center h-10 hover:bg-black/10"
+            :class="trClass(item.id)">
+            <td>{{ item.id }}</td>
+            <td>{{ item.bagian }}</td>
+            <td>{{ item.telp }}</td>
+            <td>{{ item.hari_telp }}</td>
+            <td>{{ item.jam_telp }}</td>
+            <td>{{ item.whatsapp }}</td>
+            <td>{{ item.hari_whatsapp }}</td>
+            <td>{{ item.jam_whatsapp }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     <div class="my-10">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.7546418901143!2d110.35331296803461!3d-7.709456359341933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a5f342feb9d01%3A0xb643aead9e9abb54!2sRS%20PKU%20Muhammadiyah%20Sleman!5e0!3m2!1sid!2sid!4v1744709327465!5m2!1sid!2sid"
         width="100%"
         height="450"
-        style="border: 0; border-radius: 25px;"
+        style="border: 0; border-radius: 25px"
         allowfullscreen=""
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"></iframe>
