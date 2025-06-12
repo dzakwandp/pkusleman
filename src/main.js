f990248a240460cc6396d54c24c574cc0f1976cb
Dzakwan Diego <diegodzakwan@gmail.com>
2025-06-12 14:17:50 +0700

    [refac] app: changin icon import

--- src/main.js
+++ src/main.js
@@ -17,48 +17,10 @@ import {
   faWhatsapp,
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
-import {
-  faTruckMedical,
-  faLocationDot,
-  faHospital,
-  faUserDoctor,
-  faClipboardList,
-  faArrowLeft,
-  faArrowRight,
-  faPhone,
-  faAt,
-  faCaretRight,
-  faChevronDown,
-  faChildReaching,
-  faBone,
-  faBed,
-  faBars,
-  faXmark,
-} from "@fortawesome/free-solid-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 /* add icons to the library */
-library.add(
-  faWhatsapp,
-  faInstagram,
-  faYoutube,
-  faTruckMedical,
-  faEnvelope,
-  faLocationDot,
-  faHospital,
-  faUserDoctor,
-  faClipboardList,
-  faPhone,
-  faAt,
-  faCaretRight,
-  faArrowLeft,
-  faArrowRight,
-  faChevronDown,
-  faChildReaching,
-  faBone,
-  faBed,
-  faBars,
-  faXmark
-);
+library.add(faWhatsapp, faInstagram, faYoutube, faEnvelope, fas);
 
 AOS.init();
 
