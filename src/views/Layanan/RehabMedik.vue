107b58d09ecf2df9c9c69f5a53d78f47a32d25bc
Dzakwan Diego <diegodzakwan@gmail.com>
2026-04-29 12:09:48 +0700

    [feat] add klinik rehabilitasi tumbuh kembang

--- src/views/Layanan/RehabMedik.vue
+++ src/views/Layanan/RehabMedik.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col px-4 md:px-20 mt-10 min-h-screen">
     <div class="flex flex-col w-max mx-auto gap-2">
-      <p class="text-4xl text-center text-gray-800">
+      <p v-if="data[0]" class="text-4xl text-center text-gray-800">
         {{ data[0].nama_poli }}
       </p>
       <div class="h-[2px] bg-primary-green w-full" />
